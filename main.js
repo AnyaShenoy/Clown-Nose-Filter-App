8cc7b35afcd2ef6c80ad424ad25ff8c9f2467e58
AnyaShenoy <67114305+AnyaShenoy@users.noreply.github.com>
2021-06-01 17:46:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=""
+noseY=""
+function preload(){
+    clown_image= loadImage('https://i.postimg.cc/CLyrnhLC/clown-nose.png');
+
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    video.size(300,300);
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        console.log("nose x= "+ results[0].pose.nose.x)
+        console.log("nose y= "+ results[0].pose.nose.y)
+        noseX= results[0].pose.nose.x-15;
+        noseY= results[0].pose.nose.y-10;
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    fill('#de0000');
+    stroke('#a60000');
+    //circle(noseX,noseY,20);
+    image(clown_image,noseX,noseY,30,30);
+}
+
+function take_snapshot(){
+    save('clown_filter.png');
+}
+
+
